73160b1c22bc05c88bfcd45e8750c9f655b4b139
Ionut Nohai <nohai.ionut02@gmail.com>
2023-03-15 13:36:16 +0100

    A working example of passing data from provider through an env

--- nohai_ionut02/components/alert/alert.composition.tsx
+++ nohai_ionut02/components/alert/alert.composition.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Alert } from './alert';
 import { useEnvData } from '@nohai_ionut02/nohai_ionut02.components.data-context';
+import { Alert } from './alert';
 
 export const BasicAlert = () => {
 
   const value = useEnvData();
 
-  console.log("value", value);
-
   return (
-    <Alert>hello world!</Alert>
+    <Alert>hello world!  {value}</Alert>
   );
 }
--- nohai_ionut02/envs/my-react/my-react.aspect.ts
+++
@@ -1,5 +0,0 @@
-import { Aspect } from '@teambit/harmony';
-
-export const MyReactAspect = Aspect.create({
-  id: 'nohai_ionut02.nohai_ionut02/envs/my-react',
-});
